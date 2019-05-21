296619b22db9923dfe374ac6f0b9167603d6b04a
mikedadalin <mikedadalin@gmail.com>
2019-05-21 17:07:52 -0400

    Create 152. Maximum Product Subarray.js

---
+++ 152. Maximum Product Subarray.js
@@ -0,0 +1,38 @@
+/----- JavsScript 99.73%  -----/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    
+    /*************************************************************************************
+        Use two DP arrays.
+        
+        prefix product array => [a, axb, axbxc, ...]
+        
+        f[] use to save the max value from each multiplication
+        g[] use to save the min value from each multiplication
+        
+        the reason of g[] is bc if the next nums[i] is negative number,
+        it is possible to make the min become max.
+        
+        compare f[i-1] * nums[i], g[i-1] * nums[i], nums[i]
+        to find the new max and new min
+        
+        assign the new max to the res.
+        
+    *************************************************************************************/
+            
+    let f = [nums[0]];
+    let g = [nums[0]];
+    let res = nums[0];
+    
+    for(let i = 1; i < nums.length; i++) {
+        f[i] = Math.max(f[i-1]*nums[i],g[i-1]*nums[i], nums[i]);
+        g[i] = Math.min(f[i-1]*nums[i],g[i-1]*nums[i], nums[i]);
+        
+        res = Math.max(f[i],g[i],res);        
+    }
+    
+    return res;    
+};
