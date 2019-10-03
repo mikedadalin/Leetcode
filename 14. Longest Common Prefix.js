b0ab18d57fe7734b0501a0fd9d60bf5098f9f897
Tzuhao Lin <mikedadalin@gmail.com>
2019-10-03 14:03:49 -0400

    Create 14. Longest Common Prefix.js

---
+++ 14. Longest Common Prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+
+function longestCommonPrefix(strs) {
+  if (!strs.length) return '';
+  
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let str of strs) {
+        if (str[i] !== strs[0][i]) {
+            return str.slice(0, i);
+      }
+    }
+  }
+  
+  return strs[0];
+}
