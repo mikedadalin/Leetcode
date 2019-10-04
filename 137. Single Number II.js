dcce0c93c93324e2d92cbea9ae2cd3f44ae6f0dd
Tzuhao Lin <mikedadalin@gmail.com>
2019-10-03 20:37:59 -0400

    Create 137. Single Number II.js

---
+++ 137. Single Number II.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    nums.sort(function(a,b) { return a-b });
+    
+    for(let i = 0; i < nums.length; i += 3) {
+        if(nums[i] != nums[i+1]) {
+            return nums[i];
+        } 
+    }
+};
