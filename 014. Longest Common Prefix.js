efa90b1fca0b7c9e3aa27f1b0ef5f0d6d02bc9ca
Tzuhao Lin <mikedadalin@gmail.com>
2021-08-08 20:13:42 -0400

    Update 014. Longest Common Prefix.js

--- 014. Longest Common Prefix.js
+++ 014. Longest Common Prefix.js
@@ -1,3 +1,5 @@
+// Method 1
+
 /**
  * @param {string[]} strs
  * @return {string}
@@ -17,3 +19,36 @@ function longestCommonPrefix(strs) {
   
   return strs[0];
 }
+
+
+
+// Method 2
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs.length == 1) return strs[0];
+    
+    strs.sort();
+    
+    for(let ele of strs) {
+        if(ele.length == 0) return "";
+    }        
+    
+    let matchPrefix = strs[0];
+    
+    for(let ele of strs) {
+        for(let j = 0; j < ele.length; j++) {
+            if(matchPrefix[j] != ele[j]) {
+                matchPrefix = matchPrefix.slice(0,j);
+                break;
+            }
+        }
+    }
+    
+    return matchPrefix;
+    
+    
+};
